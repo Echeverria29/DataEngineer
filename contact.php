e9a75d6eb9af39b5fb93d719435e5c030267b9f2
Echeverria29 <or.echeverria@duocuc.cl>
2024-08-18 21:54:31 -0400

    pagina 98%

--- contact.php
+++ contact.php
@@ -1,69 +1,30 @@
 <?php
+// Verificar si el formulario ha sido enviado
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Recoger los datos del formulario
+    $nombre = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $mensaje = htmlspecialchars($_POST['comment']);
 
-//Retrieve form data. 
-//GET - user submitted data using AJAX
-//POST - in case user does not support javascript, we'll use POST instead
-$name = ($_GET['name']) ? $_GET['name'] : $_POST['name'];
-$email = ($_GET['email']) ?$_GET['email'] : $_POST['email'];
-$comment = ($_GET['comment']) ?$_GET['comment'] : $_POST['comment'];
+    // Validar el formato del email
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "Formato de email inválido";
+        exit;
+    }
 
-//flag to indicate which method it uses. If POST set it to 1
+    // Configurar los parámetros del correo
+    $para = "or.echeverria@duocuc.cl"; // Reemplaza con tu dirección de correo
+    $asunto = "Mensaje del formulario de contacto de " . $nombre;
+    $contenido = "Nombre: $nombre\nEmail: $email\n\nMensaje:\n$mensaje";
+    $encabezados = "From: $email";
 
-if ($_POST) $post=1;
-
-//Simple server side validation for POST data, of course, you should validate the email
-if (!$name) $errors[count($errors)] = 'Please enter your name.';
-if (!$email) $errors[count($errors)] = 'Please enter your email.'; 
-if (!$comment) $errors[count($errors)] = 'Please enter your message.'; 
-
-//if the errors array is empty, send the mail
-if (!$errors) {
-
-	//recipient - replace your email here
-	$to = 'wowthemesnet@gmail.com';	
-	//sender - from the form
-	$from = $name . ' <' . $email . '>';
-	
-	//subject and the html message
-	$subject = 'Message via Aries from ' . $name;	
-	$message = 'Name: ' . $name . '<br/><br/>
-		       Email: ' . $email . '<br/><br/>		
-		       Message: ' . nl2br($comment) . '<br/>';
-
-	//send the mail
-	$result = sendmail($to, $subject, $message, $from);
-	
-	//if POST was used, display the message straight away
-	if ($_POST) {
-		if ($result) echo 'Thank you! We have received your message.';
-		else echo 'Sorry, unexpected error. Please try again later';
-		
-	//else if GET was used, return the boolean value so that 
-	//ajax script can react accordingly
-	//1 means success, 0 means failed
-	} else {
-		echo $result;	
-	}
-
-//if the errors array has values
+    // Enviar el correo
+    if (mail($para, $asunto, $contenido, $encabezados)) {
+        echo "¡Tu mensaje ha sido enviado. Gracias!";
+    } else {
+        echo "Hubo un error al enviar tu mensaje. Por favor, inténtalo de nuevo más tarde.";
+    }
 } else {
-	//display the errors message
-	for ($i=0; $i<count($errors); $i++) echo $errors[$i] . '<br/>';
-	echo '<a href="index.html">Back</a>';
-	exit;
+    echo "Método de solicitud inválido.";
 }
-
-
-//Simple mail function with HTML header
-function sendmail($to, $subject, $message, $from) {
-	$headers = "MIME-Version: 1.0" . "\r\n";
-	$headers .= "Content-type:text/html;charset=iso-8859-1" . "\r\n";
-	$headers .= 'From: ' . $from . "\r\n";
-	
-	$result = mail($to,$subject,$message,$headers);
-	
-	if ($result) return 1;
-	else return 0;
-}
-
 ?>
